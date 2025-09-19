981cdc6e2c742bd3bb44a3e99e76250b2ad167b2
Nils Peter Petersen <nils.petersen@bnitm.de>
2025-09-19 19:32:10 +0200

    different profiles implemented

--- virus/docker_images/push_to_hub.sh
+++ virus/docker_images/push_to_hub.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 images=(general)
+docker_user=oprgroup
 
 for img in "${images[@]}";
 do
     version=$(cat $img/version.txt)
     echo $img:$version
-    docker build --no-cache -t oprgroup/vimopdb_${img}:$version $img/
+    docker push ${docker_user}/vimopdb_${img}:$version
     echo ""
 done
