8a1f2c717059663e60a304ec3c0367eec5034bc8
Nils Petersen <nils.petersen@gaia.bnitm.de>
2025-09-30 12:02:02 +0200

    cluster job update

--- virus/cluster_job.sh
+++ virus/cluster_job.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 #SBATCH -c 8 # number of cores
 #SBATCH --mem 128000 # memory pool for all cores
-#SBATCH -o /data2/le-petersen/nils/slurmout/slurm.db_build.%N.%j.out # STDOUT
-#SBATCH -e /data2/le-petersen/nils/slurmout/slurm.db_build.%N.%j.err # STDERR
+#SBATCH -o /data2/le-petersen/shared_data/slurmout/slurm.db_build.%N.%j.out # STDOUT
+#SBATCH -e /data2/le-petersen/shared_data/slurmout/slurm.db_build.%N.%j.err # STDERR
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nils.petersen@bnitm.de
 #SBATCH --job-name=db_build
 
-source /opt/conda/etc/profile.d/conda.sh
-export PATH="/opt/conda/bin:$PATH"
-conda activate env
-
-cd /data/home/nils.petersen/dev/VirusDatasetCuration/workflow
-nextflow main.nf -c /data/home/nils.petersen/dev/VirusDatasetCuration/workflow/configs/db2.0.config
-#nextflow main.nf -c /data/home/nils.petersen/dev/VirusDatasetCuration/workflow/testset/test.config
+workflow_dir=/data2/le-petersen/shared_data/db_different_versions/VimopDatabaseUpdate/virus
+cd $workflow_dir
+config_path=configs/db2.2/db2.2.config
+nextflow main.nf -c $config_path -profile conda
