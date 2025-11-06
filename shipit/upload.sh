92f2eb38894bda508a8595b87e20b1291eed90a2
Nils Petersen <nils.petersen@gaia.bnitm.de>
2025-11-06 19:55:38 +0100

    path fix in upload

--- shipit/upload.sh
+++ shipit/upload.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-#scp -r "${VIMUPDATE_DB}/split/files" "${VIMUPDATE_FILESHARE_PREFIX}/files"
+scp -r "${VIMUPDATE_DB}/split/files/*" "${VIMUPDATE_FILESHARE_PREFIX}/files"
 scp -r "${VIMUPDATE_DB}/split/vimop_db.${VIMUPDATE_DB_VERSION}.yaml" "${VIMUPDATE_FILESHARE_PREFIX}/vimop_db.${VIMUPDATE_DB_VERSION}.yaml"
 scp -r "${VIMUPDATE_DB}/split/vimop_db.${VIMUPDATE_DB_VERSION}.yaml" "${VIMUPDATE_FILESHARE_PREFIX}/latest.yaml"
